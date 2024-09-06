e7e2cb8122819d06346e89cddc4b6539338d251a
Joel Ayansola <joel.ayansola@kane.co.uk>
2024-09-06 10:36:10 +0100

    DRIVER HEADER 06/09/2024

---
+++ STM32F030Rxx/STM32F030R8/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/jayansola.KANE/STM32Projects/STM32Project/STM32F030Rxx/STM32F030R8/Driver/Inc/stm32F030r8.h
+C:/Users/jayansola.KANE/STM32Projects/STM32Project/STM32F030Rxx/STM32F030R8/Driver/Inc/stm32F030r8.h:
